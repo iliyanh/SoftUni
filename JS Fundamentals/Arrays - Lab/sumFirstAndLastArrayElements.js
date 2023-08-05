219acc7d0ba8975230d94a4733982a2b585dd492
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-08-05 10:55:55 +0100

    Add files via upload

---
+++ JS Fundamentals/Arrays - Lab/sumFirstAndLastArrayElements.js
@@ -0,0 +1,12 @@
+function sumFirstAndLastArrayElements(arr){
+
+    let first = arr[0];
+    let last = arr[arr.length - 1];
+
+    console.log(first+last);
+
+
+}
+sumFirstAndLastArrayElements([20, 30, 40]);
+sumFirstAndLastArrayElements([10, 17, 22, 33]);
+sumFirstAndLastArrayElements([11, 58, 69]);
