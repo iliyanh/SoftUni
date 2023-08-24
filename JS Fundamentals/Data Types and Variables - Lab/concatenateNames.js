a769d5297dc8ed5a0b4f88c5e92c1af62a773a5d
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-08-24 21:24:40 +0100

    Add files via upload

---
+++ JS Fundamentals/Data Types and Variables - Lab/concatenateNames.js
@@ -0,0 +1,9 @@
+function concatenateNames(firstName, lastName, delimiter){
+
+    console.log(`${firstName}${delimiter}${lastName}`);
+
+
+}
+concatenateNames('John','Smith','->')
+concatenateNames('Jan','White','<->')
+concatenateNames('Linda','Terry','=>')
