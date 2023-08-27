c938b3e0e7833256dd900106276ff94e34823aa8
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-08-27 18:40:38 +0100

    Add files via upload

---
+++ JS Fundamentals/Data Types and Variables - Lab/requiredReading.js
@@ -0,0 +1,9 @@
+function requiredReading(bookSize, pagesPerHour, days){
+
+    let totalTime = bookSize / pagesPerHour;
+    let hoursPerDay = totalTime / days;
+    console.log(hoursPerDay);
+
+}
+requiredReading(212,20 ,2 )
+requiredReading(432,15 ,4 )
