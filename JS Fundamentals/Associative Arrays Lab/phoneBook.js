efd44922ff5a3a2360fb108790ac8c5c02d94cc9
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-10-30 18:51:33 +0000

    Add files via upload

---
+++ JS Fundamentals/Associative Arrays Lab/phoneBook.js
@@ -0,0 +1,23 @@
+function phoneBook(arr) {
+    let obj = {};
+
+    for (let line of arr) {
+        let [key, value] = line.split(" ");
+        obj[key] = value;
+    }
+    for (const key in obj) {
+
+        console.log(`${key} -> ${obj[key]}`);
+    }
+
+}
+phoneBook(['Tim 0834212554',
+    'Peter 0877547887',
+    'Bill 0896543112',
+    'Tim 0876566344']
+);
+phoneBook(['George 0552554',
+    'Peter 087587',
+    'George 0453112',
+    'Bill 0845344']
+);
