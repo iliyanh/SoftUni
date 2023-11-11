3b9207e3c18f0bc4b22804304679ed48fb6bc18c
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-11-11 19:15:52 +0000

    Add files via upload

---
+++ JS Fundamentals/Functions - Exercise/nxmMatrix.js
@@ -0,0 +1,15 @@
+function matrix(num) {
+    let buff = "";
+    for (let i = 0; i < num; i++) {
+        let secondBuff = "";
+        buff = num 
+        for(let j = 0 ; j < buff  ; j++){
+            secondBuff += num + " ";
+        }
+        console.log(secondBuff);
+    }
+
+}
+matrix(3);
+matrix(7);
+matrix(2);
