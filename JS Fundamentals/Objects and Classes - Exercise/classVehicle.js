a8b8b95fab47f5631d91933dd3699413ad4478d0
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-11-30 21:55:08 +0000

    Add files via upload

---
+++ JS Fundamentals/Objects and Classes - Exercise/classVehicle.js
@@ -0,0 +1,25 @@
+
+
+    class Vehicle {
+        constructor(type, model, parts, fuel){
+            this.type = type,
+            this.model = model,
+            this.parts = {
+                engine: parts.engine,
+                power: parts.power,
+                quality: parts.engine * parts.power
+            }
+            this.fuel = Number(fuel)
+            
+            
+        }
+        drive(fuelLoss) {
+            this.fuel -= fuelLoss;
+        }
+       
+    }
+   
+
+
+
+
