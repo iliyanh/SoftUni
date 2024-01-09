df43290d3819ed81924f21bbfd0a27c6d4b62dfb
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2024-01-09 21:22:55 +0000

    Add files via upload

---
+++ JS Fundamentals/Mid Exam/test.js
@@ -0,0 +1,33 @@
+function theAngryCat(arr, start, itemValue) {
+    let leftSum = 0;
+    let rightSum = 0;
+
+    for(let i = start - 1; i >= 0 ; i--){
+        if(itemValue === "cheap"){
+            leftSum += arr[i]
+        } else if (itemValue === "expensive"){
+            leftSum += arr[i];   
+        }
+    }
+
+    for(let j = start + 1; j <= arr.length; j++){
+        if(itemValue === "cheap"){
+            rightSum += arr[j]
+        } else if (itemValue === "expensive"){
+            rightSum += arr[j];    
+        }
+    }
+
+    if (leftSum === rightSum || leftSum > rightSum){
+        console.log(`Left - ${leftSum}`);
+    } else {
+        console.log(`Right - ${rightSum}`);
+    }
+    
+
+    
+
+}
+theAngryCat([1, 5, 1],1,"cheap");
+theAngryCat([5, 10, 12, 5, 4, 20],3,"cheap");
+theAngryCat([-2, 2, 1, 5, 9, 3, 2, -2, 1, -1, -3, 3],7,"expensive");
