c04a78e3dc9255c04bcc27359b9b9a7e5bb8cd0f
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2024-02-22 20:17:02 +0000

    Add files via upload

---
+++ JS Advanced/Lab Objects & Composition/cityRecord.js
@@ -0,0 +1,18 @@
+function cityRecord(name, population, treasury) {
+
+    let output = {
+        name: name,
+        population: population,
+        treasury: treasury
+    }
+
+    return output
+
+
+}
+cityRecord('Tortuga',
+    7000,
+    15000);
+cityRecord('Santo Domingo',
+    12000,
+    23500);
